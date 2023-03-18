02c5f85836bf4a0a6ae2e061f9a20e1f0072023e
ALEKSANDER <melsmaxms@gmail.com>
2023-03-18 22:46:02 +0700

    add code and tests

---
+++ src/test/java/GameStoreTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class GameStoreTest {
+
+    @Test
+    public void shouldAddGame() {
+
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+
+        Assertions.assertTrue(store.containsGame(game));
+    }
+}
